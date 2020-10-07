746f219a573bd52a66931ac8c787cba7ded82fc8
Akshay Nair <phenax5@gmail.com>
2020-10-07 19:50:10 +0530

    Replaces celular automata banner with balls

---
+++ cellular-automata/README.md
@@ -0,0 +1,1 @@
+[![Akshay Nair](https://raw.githubusercontent.com/phenax/phenax/master/banner.svg)](https://phenax.github.io)
---
+++ cellular-automata/generate-svg.js
@@ -0,0 +1,83 @@
+const COLOR_ACCENT = '#58c9b9';
+const COLOR_BLOCK = COLOR_ACCENT;
+const COLOR_PRIMARY = '#151823';
+const COLOR_FG = '#454853';
+
+const getNextRow = (row, rule) => row.map((current, i) => {
+  const prev = row[i - 1] || false;
+  const next = row[i + 1] || false;
+  return rule(prev, current, next);
+});
+
+const rules = {
+  150: (a, b, c) => (a + b + c) % 2 === 1,
+  126: (a, b, c) => !(a === b && b === c),
+  60: (a, b, _) => a === b,
+};
+const RULE = rules[126];
+
+const getInitialRow = count => Array(count).fill(false).map(() => Math.random() > 0.5);
+
+const SIZE = 25;
+const GAP = 5;
+const drawRow = (current, row) => current.map((state, col) => {
+  const x = col * (SIZE + GAP);
+  const y = row * (SIZE + GAP);
+
+  return !state ? '' : `
+    <rect
+        x="${x}"
+        y="${y}"
+        class="b"
+        style="animation-delay: ${row * col / 50}s;"
+    />
+  `.replace(/\s+/g, ' ');
+}).join('');
+
+const MAX_ROWS = 20;
+const renderSvg = (rule, currentRow, rows) => {
+  if (rows > MAX_ROWS) return '';
+  return drawRow(currentRow, rows) + renderSvg(rule, getNextRow(currentRow, rule), rows + 1);
+};
+
+const width = 1500;
+const height = (SIZE + GAP) * MAX_ROWS;
+
+const fontSize = 90;
+const padding = (height - fontSize); // TODO
+
+const template = ({ contents }) => `
+<svg viewBox="0 0 ${width} ${height}" width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+  ${contents}
+  <text
+    fill="black"
+    xml:space="preserve"
+    font-family="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif"
+    font-size="${fontSize}"
+    font-weight="bold"
+    letter-spacing="0em"
+  >
+    <tspan x="${width/2 - 3.29 * fontSize}" y="${height/2 + fontSize * 0.231}" fill="${COLOR_FG}">Akshay <tspan fill="${COLOR_ACCENT}">Nair</tspan></tspan>
+  </text>
+  <style>
+    @keyframes animation {
+      0% { opacity: 0.2; transform: scale(1); }
+      50% { opacity: 0.5; transform: scale(1.01); }
+      100% { opacity: 0.2; transform: scale(1); }
+    }
+    .b {
+      fill: ${COLOR_BLOCK};
+      width: ${SIZE}px;
+      height: ${SIZE}px;
+      transform-origin: 50% 50%;
+      opacity: 0.2;
+      animation: animation 2s infinite ease-in-out;
+    }
+  </style>
+</svg>
+`.replace(/\s+/g, ' ');
+
+const contents = renderSvg(RULE, getInitialRow(width / SIZE), 0);
+
+console.log(template({ contents }));
+
